8a7a4ef25c499731a205d3a4fcb20dcded674389
Joseph Rios Henao <joseph232005@gmail.com>
2020-11-06 16:04:14 -0500

    Add practices with python

---
+++ HackerRank/Python/printFuction.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input())
+
+Cadena = ""    
+for i in range(n):
+    Cadena += str(i+1)
+print(Cadena)
--- HackerRank/Python/pruebas.py
+++ HackerRank/Python/pruebas.py
@@ -1,2 +1,18 @@
-n = 15
-print(range(n))
+def is_leap(year):
+    leap = False
+    
+    if (year%4):
+        if (year%100==0):
+            if (year%400==0):
+                leap=True
+            else:
+                leap = True
+        else:
+            leap=True
+    else:
+        leap = True
+
+    return leap
+
+year = int(input())
+print(is_leap(year))
