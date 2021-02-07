01932529e5adbd0745aecad15233f7f190df89b9
Joseph Rios Henao <joseph232005@gmail.com>
2021-02-07 09:28:13 -0500

    Terminando problema pero no entendi nada.....

--- HackerRank/Python/Easy/nestedLists.py
+++ HackerRank/Python/Easy/nestedLists.py
@@ -1,4 +1,4 @@
-listScore = []
+"""listScore = []
 listNameScore = []
 listLowNameScore = []
 if __name__ == '__main__':
@@ -14,3 +14,10 @@ if __name__ == '__main__':
                 listLowNameScore.append([listNameScore(j,0),listNameScore(j,1)])
     print(listNameScore)
     #print(sorted(listNameScore)) #Lista ordenada por nombres alphabeticamente juntu a su puntuacion
+"""
+Students = []
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        Students.append([input(),float(input())])
+    second_highest = sorted(list(set([marks for name, marks in Students])))[1]
+    print('\n'.join([a for a,b in sorted(Students) if b == second_highest]))
