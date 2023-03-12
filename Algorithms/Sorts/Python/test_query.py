397467af74d048347311e483e31ab55c13417ad6
David Henao Martínez <50639656+Dhliv@users.noreply.github.com>
2023-03-12 13:24:07 -0500

    Update test_query.py
    Use asserts to make tests

--- Algorithms/Sorts/Python/test_query.py
+++ Algorithms/Sorts/Python/test_query.py
@@ -2,6 +2,4 @@ from typing import List
 
 def test_generic(data:List[int]):
     for idx, number in enumerate(data):
-        if number < data[idx+1]: break
-        return True
-    return False
+        assert number < data[idx+1]
